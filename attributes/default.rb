a570c91aefb4d0d4b9e2e306e96f3f8fcf9c7368
John Bellone <jbellone@bloomberg.net>
2015-08-16 22:52:39 -0400

    Adds binary, version and checksum properties.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,8 +10,9 @@ default['cassandra-cluster']['service_name'] = 'cassandra'
 default['cassandra-cluster']['service_user'] = 'cassandra'
 default['cassandra-cluster']['service_group'] = 'cassandra'
 
+default['cassandra-cluster']['service']['environment']['JMX_PORT'] = 9010
 default['cassandra-cluster']['service']['environment']['MX4J_PORT'] = 8081
 default['cassandra-cluster']['service']['environment']['MX4J_ADDRESS'] = '-Dmx4jaddress=0.0.0.0'
-default['cassandra-cluster']['service']['version'] = ''
-default['cassandra-cluster']['service']['binary_checksum'] = ''
-default['cassandra-cluster']['service']['binary_url'] = "" # rubocop:disable Style/StringLiterals
+default['cassandra-cluster']['service']['version'] = '2.2.0'
+default['cassandra-cluster']['service']['binary_checksum'] = '6405eb063e7c8a44a485ac12b305c00ad62c526cc021bcce145c29423ae7b0a2'
+default['cassandra-cluster']['service']['binary_url'] = "http://apache.cs.utah.edu/cassandra/%(version)/apache-cassandra-%(version)-bin.tar.gz" # rubocop:disable Style/StringLiterals
